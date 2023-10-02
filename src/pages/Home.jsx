dc4375348a2f4b7e83228a735348c883bf41e787
unknown <sohailshahidi1435@gmail.com>
2023-10-02 18:06:44 +0430

    Refactor fetching data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,8 @@
-import { useState, useEffect } from "react"
 import BlogList from "../components/BlogList"
+import useFetch from "../components/useFetch"
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/blogs')
-      .then(res => {
-        if (!res.ok) {
-          throw Error ('Could not fetch the data for that resource');
-        }
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch(err => {
-        setIsPending(false);
-        setError(err.message);
-      })
-  }, []);
+  const { data: blogs, isPending, error } = useFetch('http://localhost:3000/blogs');
 
   return (
     <section>
