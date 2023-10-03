1d7b338935be08bb9170d00158f1dd23a903ec1f
unknown <sohailshahidi1435@gmail.com>
2023-10-03 18:19:36 +0430

    Add BlogDetails styling

--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -6,12 +6,12 @@ const BlogDetails = () => {
   const {data: blog, isPending, error} = useFetch('http://localhost:3000/blogs/' + id)
 
   return (
-    <section>
+    <section className="flex justify-center items-center">
       { isPending && <div>Loading</div> }
       { error && <div>{ error }</div> }
       { blog && (
-        <article>
-          <h2>{ blog.title }</h2>
+        <article className="flex flex-col gap-5 w-2/3 mt-14">
+          <h2 className='text-2xl text-red-500'>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
           <p>{ blog.body }</p>
         </article>
