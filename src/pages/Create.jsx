5fde114c6ab0e8093ca946de95809579e10bfabe
unknown <sohailshahidi1435@gmail.com>
2023-10-03 19:10:37 +0430

    Add styles to Create blog form

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -4,22 +4,23 @@ const Create = () => {
   return (
     <section className="flex flex-col justify-center items-center gap-5">
       <h2 className='text-3xl text-red-500 mt-14'>Add a New Blog</h2>
-      <form>
+      <form className='flex flex-col justify-center gap-3 w-1/3'>
         <label>Blog Title:</label>
         <input 
           type="text"
+          className='border rounded-sm px-2 py-1'
           required
         />
         <label>Blog Body:</label>
-        <textarea required>
+        <textarea className='border rounded-sm px-2 py-1' required>
 
         </textarea>
         <label>Blog Author:</label>
-        <select>
+        <select className='border rounded-sm px-2 py-1'>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        <button className='self-center bg-red-500 text-white w-20 rounded-md px-1 py-2'>Add Blog</button>
       </form>
     </section>
   )
