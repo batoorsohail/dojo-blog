502103a83093421094b8088b927e7d75376b2a81
unknown <sohailshahidi1435@gmail.com>
2023-10-04 18:23:52 +0430

    Update Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Create from './pages/Create';
 import BlogDetails from './pages/BlogDetails'
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/create' element={<Create />} />
         <Route path='/blogs/:id' element={<BlogDetails />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   );
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <section className="flex flex-col items-center">
-      <h1 className='text-2xl text-gray-500'>Sorry!</h1>
-      <p>The page Not Found</p>
-      <Link to="/" className="text-gray-500">
-        Back to Home page
-      </Link>
+    <section className="flex flex-col justify-center items-center">
+      <div className="mt-14">
+        <h1 className='text-2xl text-gray-500'>Sorry!</h1>
+        <p>The page Not Found</p>
+        <Link to="/" className="text-gray-500">
+          Back to Home page
+        </Link>
+      </div>
     </section>
   )
 }
